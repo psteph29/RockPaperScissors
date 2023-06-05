d9f9064fba5fb57ccabfe6a898a9ae50320c9cf4
Paige Stephenson <paigestephenson29@gmail.com>
2023-06-05 07:51:44 -0600

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Paige Stephenson on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
